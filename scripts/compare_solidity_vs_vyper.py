cf294dbc0cc1eeeb123d876f2705edc63f25f2fe
Gerrit Hall <gerrit@rezscore.com>
2022-04-26 12:26:28 -0700

    init: First Commit

---
+++ scripts/compare_solidity_vs_vyper.py
@@ -0,0 +1,23 @@
+import brownie
+from brownie import *
+
+def main():
+    solidity = Vanilla721A.deploy({'from': a[0]})
+    vyper = VyperNFT.deploy({'from': a[0]})
+
+    print("Deploy Cost")
+    print(f"Solidity: {gas_cost(history[-2])}")
+    print(f"Vyper: {gas_cost(history[-1])}")
+
+    solidity.mint(1, {'from': a[0], 'value': Wei('.1 ether')})
+    vyper.mint(a[0], {'from': a[0], 'value': Wei('.1 ether')})
+
+    print("\nMint Cost")
+    print(f"Solidity: {gas_cost(history[-2])}")
+    print(f"Vyper: {gas_cost(history[-1])}")
+
+
+
+
+def gas_cost(tx):
+    return tx.gas_used * Wei('50 gwei') / 10 ** 18
